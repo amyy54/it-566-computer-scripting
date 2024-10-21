1bd848f84688553170764eb20aacb2689ccc9cd9
Amy C <git@amyy.me>
2024-10-21 19:46:12 -0400

    Format all Python code with black

--- python/clean_architecture/tests/test_example.py
+++ python/clean_architecture/tests/test_example.py
@@ -1,22 +1,18 @@
 import pytest
-#from context import Example
+
+# from context import Example
+
 
 class Test_Example:
-	pass
+    pass
 
-	# def test_increment(self):
-	# 	example = Example()
-	# 	assert example.get_count() == 0
-	# 	example.increment_count()
-	# 	assert example.get_count() == 1
+    # def test_increment(self):
+    # 	example = Example()
+    # 	assert example.get_count() == 0
+    # 	example.increment_count()
+    # 	assert example.get_count() == 1
 
-
-	# def test_sum(self):
-	# 	example = Example()
-	# 	assert example.sum(1, 1) == 2
-	# 	assert example.sum(1,2) == 3
-
-		
-
-
-
+    # def test_sum(self):
+    # 	example = Example()
+    # 	assert example.sum(1, 1) == 2
+    # 	assert example.sum(1,2) == 3
